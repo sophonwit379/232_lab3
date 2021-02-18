dc6972d838c7766ddd3e9833c1c4377db25a69d0
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-02-18 17:30:58 +0700

    com

--- 6320503114_3_5.c
+++ 6320503114_3_5.c
@@ -1,14 +1,26 @@
 #include<stdio.h>
 int main()
 {
-    int n,i=2,j;
+    int n,i=2,tmp,show,count=0;
     scanf("%d",&n);
+    show=n;
     while(n!=0)
     {
         if(n%i==0)
         {
             n/=i;
-            if(n)
+            if(tmp==i || count>3)
+            {
+                printf("%d is not a Lucky Number.",show);
+                break;
+            }
+            if(tmp!=i && count<=3 && n==1)
+            {
+                printf("%d a Lucky Number.",show);
+                break;
+            }
+            tmp=i;
+            count++;
         }
         else
         {
@@ -16,8 +28,5 @@ int main()
         }
 
     }
-
-
-
 
 }
