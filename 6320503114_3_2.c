12758ac8e80e149c6c1136c8b3ad94babb6eeb1c
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-02-18 15:51:15 +0700

    condition and output

--- 6320503114_3_2.c
+++ 6320503114_3_2.c
@@ -1,14 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int i,k,n;
+    int i,max=0,n,tmp;
     scanf("%d",&n);
     int p[n],a[n];
     for(i=0;i<n;i++)
         scanf("%d %d",&p[i],&a[i]);
-
-
-
-
+    for(i=0;i<n;i++)
+    {
+        if(p[i]==p[i+1])
+            a[i]+=a[i+1];
+        if(a[i]>max)
+        {
+            max=a[i];
+            tmp=i+1;
+        }
+    }
+    printf("%d %d",tmp,max);
 
 }
