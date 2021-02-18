af399325c33530d8004d3f961512323e23566b40
sophonwit379 <78529993+sophonwit379@users.noreply.github.com>
2021-02-18 14:54:54 +0700

    find max

--- 6320503114_3_1.c
+++ 6320503114_3_1.c
@@ -1,13 +1,24 @@
 #include<stdio.h>
 int main()
 {
-    int i,j;
-    int com[5][4];
+    int i,j,max;
+    int com[5][4],tmp[5]={0};
     for(i=0;i<5;i++)
     {
         for(j=0;j<4;j++)
+        {
             scanf("%d",&com[i][j]);
+            tmp[i]+=com[i][j];
+        }
     }
+    max=tmp[0];
+    for(i=1;i<5;i++)
+    {
+        if(max>tmp[i])
+            max=tmp[i];
+
+    }
+
 
 
 
